03fefc8209320254eb50e55a89d3db7105d0fd95
Ørjan Jacobsen <orjan@orrie.org>
2022-05-16 14:46:52 +0200

    Added bulk pcap parser

---
+++ scripts/bulk_pcap_parser.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# Parse PCAP to CSV
+PARSER_PATH=$1
+PCAP_DIR=$2
+
+if [ -z $PCAP_DIR ] && [ -z $PARSER_PATH ]; then
+  echo "usage: bash $0 <path to pcap parser> <pcap directory>";
+  exit
+fi
+
+for FILE in $(find $PCAP_DIR -name "*cap" -type f); do
+  FILENAME=$(basename $FILE)
+  DIRNAME=$(dirname $FILE)
+
+  python3 $PARSER_PATH/pcap_argv_parser.py $FILE
+done
